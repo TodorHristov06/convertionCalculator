5a06b229bc0abca5b073e200a16d21c0d71852dd
TodorHristov06 <todorhristov2006@gmail.com>
2023-10-10 21:19:38 +0300

    Add project files.

---
+++ convertionCalculator/NumberConverter.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace convertionCalculator
+{
+    internal class NumberConverter
+    {
+        public static int BinaryToDecimal(string binaryNumber)
+        {
+            int num = int.Parse(binaryNumber);
+            int decVal = 0;
+            int baseVal = 1;
+
+            while (num > 0)
+            {
+                int rem = num % 10;
+                decVal += rem * baseVal;
+                num = num / 10;
+                baseVal = baseVal * 2;
+            }
+
+            return decVal;
+        }
+
+        public static int OctalToDecimal(string octalNumber)
+        {
+            int num = int.Parse(octalNumber);
+            int decVal = 0;
+            int baseVal = 1;
+
+            while (num > 0)
+            {
+                int rem = num % 10;
+                decVal += rem * baseVal;
+                num = num / 10;
+                baseVal = baseVal * 8;
+            }
+
+            return decVal;
+        }
+
+        public static int HexadecimalToDecimal(string hexadecimalNumber)
+        {
+            int decVal = 0;
+            int baseVal = 1;
+
+            for (int i = hexadecimalNumber.Length - 1; i >= 0; i--)
+            {
+                char hexChar = hexadecimalNumber[i];
+                int hexDigit = (hexChar >= '0' && hexChar <= '9') ? hexChar - '0' : (hexChar - 'A' + 10);
+                decVal += hexDigit * baseVal;
+                baseVal *= 16;
+            }
+
+            return decVal;
+        }
+
+        public static string DecimalToBinary(int decimalNumber)
+        {
+            string binaryResult = "";
+            while (decimalNumber > 0)
+            {
+                int remainder = decimalNumber % 2;
+                binaryResult = remainder.ToString() + binaryResult;
+                decimalNumber /= 2;
+            }
+            return binaryResult;
+        }
+
+        public static string DecimalToOctal(int decimalNumber)
+        {
+            string octalResult = "";
+            while (decimalNumber > 0)
+            {
+                int remainder = decimalNumber % 8;
+                octalResult = remainder.ToString() + octalResult;
+                decimalNumber /= 8;
+            }
+            return octalResult;
+        }
+
+        public static string DecimalToHexadecimal(int decimalNumber)
+        {
+            string hexResult = "";
+            while (decimalNumber > 0)
+            {
+                int remainder = decimalNumber % 16;
+                if (remainder < 10)
+                    hexResult = remainder.ToString() + hexResult;
+                else
+                    hexResult = (char)(remainder - 10 + 'A') + hexResult;
+                decimalNumber /= 16;
+            }
+            return hexResult;
+        }
+    }
+}
---
+++ convertionCalculator/Program.cs
@@ -0,0 +1,88 @@
+﻿using convertionCalculator;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.InputEncoding = Encoding.UTF8;
+
+static int GetBaseChoice()
+{
+    int choice;
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out choice) && choice >= 1 && choice <= 3)
+        {
+            return choice;
+        }
+        else
+        {
+            Console.WriteLine("Моля, въведете валиден избор.");
+        }
+    }
+}
+
+Console.WriteLine("Добре дошли в конвертора на бройни системи!");
+Console.WriteLine("Моля, изберете бройна система за вход:");
+Console.WriteLine("1. Двоична");
+Console.WriteLine("2. Десетична");
+Console.WriteLine("3. Осмична");
+/*Console.WriteLine("4. Шестнадесетична");*/
+
+int inputBase = GetBaseChoice();
+
+Console.WriteLine("Въведете числото за конвертиране:");
+string inputNumber = Console.ReadLine();
+
+int decimalNumber;
+
+switch (inputBase)
+{
+    case 1: // Двоична
+        decimalNumber = NumberConverter.BinaryToDecimal(inputNumber);
+        break;
+    case 2: // Десетична
+        decimalNumber = int.Parse(inputNumber);
+        break;
+    case 3: // Осмична
+        decimalNumber = NumberConverter.OctalToDecimal(inputNumber);
+        break;
+    case 4: // Шестнадесетична
+        decimalNumber = NumberConverter.HexadecimalToDecimal(inputNumber);
+        break;
+    default:
+        Console.WriteLine("Невалиден избор.");
+        return;
+}
+
+Console.WriteLine("Моля, изберете бройна система за изход:");
+Console.WriteLine("1. Двоична");
+Console.WriteLine("2. Десетична");
+Console.WriteLine("3. Осмична");
+Console.WriteLine("4. Шестнадесетична");
+
+int outputBase = GetBaseChoice();
+
+string result;
+
+switch (outputBase)
+{
+    case 1: // Двоична
+        result = NumberConverter.DecimalToBinary(decimalNumber);
+        break;
+    case 2: // Десетична
+        result = decimalNumber.ToString();
+        break;
+    case 3: // Осмична
+        result = NumberConverter.DecimalToOctal(decimalNumber);
+        break;
+    case 4: // Шестнадесетична
+        result = NumberConverter.DecimalToHexadecimal(decimalNumber);
+        break;
+    default:
+        Console.WriteLine("Невалиден избор.");
+        return;
+}
+
+Console.WriteLine($"Резултатът е: {result}");
+
+
+
